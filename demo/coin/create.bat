a18622061fa86fd680b23cc762d1db90aa37c4db
Jie Yang <topelinux@gmail.com>
2018-06-27 09:18:36 +0800

    修复错误命令

--- demo/coin/create.bat
+++ demo/coin/create.bat
@@ -1,4 +1,4 @@
-node ./dist/src/client/host/host.js miner create ^
+node ./dist/src/client/host/host.js create ^
 --consensus pow ^
 --handler "./dist/demo/coin/contract/handler.js" ^
 --dataDir "./demo/coin/genesis"
