88cf56943743366159e80a28a22bc9904d66fe08
yango3 <o3.yang@hotmail.com>
2019-04-10 14:51:48 +0800

    Add getBancorTokenParams to speed up

--- ruff/dposbft/chain/handler.ts
+++ ruff/dposbft/chain/handler.ts
@@ -746,6 +746,54 @@ export function registerHandler(handler: ValueHandler) {
         return await getTokenBalance(balancekv.kv!, params.tokenid.toUpperCase());
     });
 
+    // Yang Jun 2019-4-10
+    handler.addViewMethod('getBancorTokenParams', async (context: DposViewContext, params: any): Promise<{ F: BigNumber, S: BigNumber, R: BigNumber } | number> => {
+
+        // let outputError = { F: new BigNumber(0), S: new BigNumber(0), R: new BigNumber(0) };
+        if (!params.tokenid || !bCheckTokenid(params.tokenid)) {
+            return ErrorCode.RESULT_WRONG_ARG;
+        }
+        let tokenIdUpperCase = params.tokenid.toUpperCase();
+
+        // get F
+        let kvFactor = await context.storage.getReadableKeyValueWithDbname(Chain.dbBancor, Chain.kvFactor);
+        if (kvFactor.err) {
+            context.logger.error('getbancortokenparams() fail open kvFactor');
+            return ErrorCode.RESULT_DB_TABLE_OPEN_FAILED;
+        }
+        let retFactor = await kvFactor.kv!.get(tokenIdUpperCase);
+        if (retFactor.err) {
+            return ErrorCode.RESULT_DB_RECORD_EMPTY;
+        }
+        let Factor = new BigNumber(retFactor.value);
+
+        // get S
+        let kvSupply = await context.storage.getReadableKeyValueWithDbname(Chain.dbBancor, Chain.kvSupply);
+        if (kvSupply.err) {
+            context.logger.error('getbancortokenparams() fail open kvSupply');
+            return ErrorCode.RESULT_DB_TABLE_OPEN_FAILED;
+        }
+
+        let retSupply = await kvSupply.kv!.get(tokenIdUpperCase);
+        if (retSupply.err) { return ErrorCode.RESULT_DB_RECORD_EMPTY; }
+
+        let Supply = new BigNumber(retSupply.value);
+
+        // get R
+        let kvReserve = await context.storage.getReadableKeyValueWithDbname(Chain.dbBancor, Chain.kvReserve);
+        if (kvReserve.err) {
+            context.logger.error('getbancortokenparams() fail open kvReserve'); return ErrorCode.RESULT_DB_TABLE_OPEN_FAILED;
+        }
+
+        let retReserve = await kvReserve.kv!.get(tokenIdUpperCase);
+        if (retReserve.err) { return ErrorCode.RESULT_DB_RECORD_EMPTY; }
+
+        let Reserve = new BigNumber(retReserve.value);
+        // console.log('Yang-- R:', R.toString());
+
+        return { F: Factor, S: Supply, R: Reserve };
+    });
+
     handler.addViewMethod('getZeroBalance', async (context: DposViewContext, params: any): Promise<BigNumber> => {
 
         return await context.getBalance('0');
