a9e9f345ff2bd689c6c62117dc517c3a01a08420
Jie Yang <jie.yang@nanchao.org>
2019-06-24 15:43:50 +0800

    Add creat user by --num and --output

--- src/tool/address.ts
+++ src/tool/address.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import * as process from 'process';
+import * as path from 'path';
+import * as fs from 'fs';
 import {createKeyPair, addressFromSecretKey, publicKeyFromSecretKey, addressFromPublicKey, initLogger} from '../host';
 import {initUnhandledRejection, parseCommand} from '../common/';
 
@@ -8,14 +10,38 @@ initUnhandledRejection(initLogger({loggerOptions: {console: true}}));
 function main() {
     let command = parseCommand(process.argv);
     if (!command || !command.command) {
-        console.log(`Usage: node address.js <create | convert> {--secret {secret} | --pubkey {pubkey}}`);
+        console.log(`Usage: node address.js <create | convert> {--num {num} --output {filename}} | {--secret {secret} | --pubkey {pubkey}}`);
         process.exit();
     }
 
     if (command!.command === 'create') {
-        let [key, secret] = createKeyPair();
-        let addr = addressFromSecretKey(secret);
-        console.log(`address:${addr} secret:${secret.toString('hex')}`);
+       if (command!.options.has('num')) {
+            let num = command!.options.get('num');
+            let addrs = [];
+            for (let i = 0; i < num; i++) {
+                let [key, secret] = createKeyPair();
+                let addr = addressFromSecretKey(secret);
+
+                addrs.push({
+                    address: addr,
+                    secret: secret.toString('hex')
+                });
+            }
+            let users = {users:addrs};
+            if (command!.options.has('output')) {
+                let outputFilename = command!.options.get('output');
+                if (!path.isAbsolute(outputFilename)) {
+                    outputFilename = path.join(process.cwd(), outputFilename);
+                }
+                fs.writeFileSync(outputFilename, JSON.stringify(users, null, 2));
+            } else {
+                console.log(JSON.stringify(users, null, 2));
+            }
+        } else {
+            let [key, secret] = createKeyPair();
+            let addr = addressFromSecretKey(secret);
+            console.log(`address:${addr} secret:${secret.toString('hex')}`);
+        }
         process.exit();
     } else {
         if (command!.options.has('secret')) {
