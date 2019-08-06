f0e58568436a15b20b5ab1b5e6e51d9b3fd5707f
Jie Yang <jie.yang@nanchao.org>
2019-08-06 16:34:32 +0800

    Add missing file

---
+++ src/common/wal_util.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs-extra';
+
+export function remove_db_files(dbPath: string) {
+    let toRemove = [
+        dbPath,
+        `${dbPath}-wal`
+    ]
+    toRemove.forEach((item) => {
+        if (fs.existsSync(item)) {
+            fs.unlinkSync(item);
+        }
+    });
+}
+
+export function copyDBFileSync(src: string, dest: string) {
+    fs.copyFileSync(src, dest);
+    if (fs.existsSync(`${src}-wal`)) {
+        fs.copyFileSync(`${src}-wal`, `${dest}-wal`);
+    }
+}
