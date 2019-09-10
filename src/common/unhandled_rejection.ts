94ba91c10c149f5fc9712affe2a3f41cde738dd9
yango3 <o3.yang@hotmail.com>
2019-09-10 18:01:18 +0800

    update common/unhandle_rejection.ts

--- src/common/unhandled_rejection.ts
+++ src/common/unhandled_rejection.ts
@@ -3,17 +3,17 @@ import * as process from 'process';
 import { LoggerInstance } from 'winston';
 
 export function init(logger: LoggerInstance) {
-    process.on('unhandledRejection', (reason, p) => {
+    process.on('unhandledRejection', (reason: any, p) => {
         console.log('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
         logger.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
         process.exit(-1);
     });
-    
+
     process.on('uncaughtException', (err) => {
         console.log('uncaught exception at: ', err.stack);
         logger.error('uncaught exception at: ', err.stack);
         process.exit(-1);
-    });    
+    });
     process.on('beforeExit', () => {
         console.log(`beforeExit fired, no exist event loop`);
     });
