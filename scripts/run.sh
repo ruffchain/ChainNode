59f0dcd5ffec49d8fd66fd6897e48fbab4648cc6
yango3 <o3.yang@hotmail.com>
2020-03-23 14:47:32 +0800

    Modify run.sh to add --max-old-space-size config

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,11 +13,18 @@ npm run build
 echo 'hello'
 echo $*
 
-if [  "$1" = 'miner'  -o  "$1" = 'peer'  ]
-then
-    node --max-old-space-size=2048 ./dist/blockchain-sdk/src/tool/host.js $*
+ifnum= echo "$1" | grep '[^0-9]'
+num="$1"
+
+if [ "$1" = 'miner' -o "$1" = 'peer' ]; then
+    node --max-old-space-size=1024 ./dist/blockchain-sdk/src/tool/host.js $*
+elif [ -z "$ifnum" ]; then
+    echo "Number:" "$1"
+    shift
+    echo $*
+    node --max-old-space-size="$num" ./dist/blockchain-sdk/src/tool/host.js $*
 else
     echo 'Wrong arguments num'
     echo 'hello ending'
-    /bin/bash
+    # /bin/bash
 fi
