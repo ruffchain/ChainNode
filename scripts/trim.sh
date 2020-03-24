965b04af340041f9e61313ab5c6905ce716ca0e0
yango3 <o3.yang@hotmail.com>
2020-03-24 10:11:43 +0800

    trim.sh add node RAM space config

--- scripts/trim.sh
+++ scripts/trim.sh
@@ -10,5 +10,18 @@ pwd
 
 echo $*
 
-node ./dist/blockchain-sdk/src/tool/trim.js $*
+ifnum= echo "$1" | grep '[^0-9]'
+num="$1"
 
+# node ./dist/blockchain-sdk/src/tool/trim.js $*
+if [ "$1" = 'trim' -o "$1" = 'check' ]; then
+    node --max-old-space-size=1024 ./dist/blockchain-sdk/src/tool/trim.js $*
+elif [ -z "$ifnum" ]; then
+    echo "Number:" "$1"
+    shift
+    echo $*
+    node --max-old-space-size="$num" ./dist/blockchain-sdk/src/tool/trim.js $*
+else
+    echo 'Wrong arguments num'
+    echo 'hello ending'
+fi
